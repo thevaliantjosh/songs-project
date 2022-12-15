6ef37bf451155cfd28b92bed5f37fee333e12288
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-12-15 15:28:15 -0600

    Created Songs_project html, Songs Project js, Using Examples from awicks44 github for JS Spotify API

--- js/bored_api.js
+++ js/bored_api.js
@@ -22,8 +22,15 @@
 
 const getActivity = async () => {
    try {
+       let fetchOptions = {
+           method: 'GET',
+           headers: {
+               'Authorization' : `Bearer ${SPOTIFY_ACCESS_TOKEN}`,
+               'Content-Type' : 'application/json'
+           }
+       }
         const bored_api = `https://www.boredapi.com/api/activity`;
-        const response = await fetch(bored_api);
+        const response = await fetch(bored_api, fetchOptions);
         console.log(response.ok)
         const data = await response.json();
         console.log(data);
